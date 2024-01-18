9aa64383efaaa3870f8a5c7cdd59b74769ac27fb
mmesander <mark_mesander@hotmail.com>
2024-01-18 14:35:04 +0100

    add lombok and add test

---
+++ src/test/java/com/example/les17/model/OrderTest.java
@@ -0,0 +1,25 @@
+package com.example.les17.model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderTest {
+
+    @Test
+    @DisplayName("Should be good amount")
+    void calculateAmount() {
+        // arrange
+        Order order = new Order("Corsair Vengeance K75 Lux", 199.99, 4);
+        double expectedResult = 199.99 * 4;
+
+        // act
+        double unitprice = order.getUnitprice();
+        double amount = order.getQuantity();
+        double result = unitprice * amount;
+
+        // assert
+        assertEquals(expectedResult, result);
+    }
+}
